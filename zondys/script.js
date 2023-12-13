80923932e6b453d2e907b69f57f83e667ff700a4
esotericAltruist <clickforFREEkittens@gmail.com>
2023-12-13 14:46:47 -0500

    Added (rough) dynamic screen rescaling

--- zondys/script.js
+++ zondys/script.js
@@ -1,3 +1,18 @@
+function getDeviceStyle() { 
+    // Obtains an array of all <link> 
+    // elements. 
+    // Select your element using indexing. 
+    var theme = document.getElementsByTagName('link')[0]; 
+
+    // Change the value of href attribute  
+    // to change the css sheet. 
+    if (theme.getAttribute('href') == 'style.css') { 
+        theme.setAttribute('href', 'style.css'); 
+    } else { 
+        theme.setAttribute('href', 'desktop-style.css'); 
+    } 
+} 
+
 function changeSlide() {
     var index = 1
     var slides = document.getElementsByClassName("carousel-img");
